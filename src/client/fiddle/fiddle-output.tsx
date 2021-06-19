d2a88effb507acbba4991adb97497ee1b4c9271c
Shogo Wada <shogowada@users.noreply.github.com>
2021-06-19 05:59:03 -0500

    Support logging

--- src/client/fiddle/fiddle-output.tsx
+++ src/client/fiddle/fiddle-output.tsx
@@ -4,35 +4,72 @@ interface Props {
   code: string;
 }
 
+const createConsoleLite = (onLog: (...args: any[]) => void) => {
+  return ["info", "log", "warn", "error", "debug"].reduce(
+    (consoleLite, methodName) => {
+      return {
+        ...consoleLite,
+        [methodName]: onLog,
+      };
+    },
+    {}
+  );
+};
+
+const mapLogArgsToLog = (...args: any[]): string | null => {
+  if (args.length === 0) {
+    return null;
+  } else if (args.length === 1) {
+    return mapLogArgToLog(args[0]);
+  } else {
+    return args.map(mapLogArgToLog).join(" ");
+  }
+};
+
+const mapLogArgToLog = (arg: any) => {
+  if (typeof arg === "object") {
+    return JSON.stringify(arg);
+  } else {
+    return String(arg);
+  }
+};
+
+const createFunctionCode = (code: string) => {
+  return `"use strict";
+return (console) => {
+  ${code}
+};`;
+};
+
 export const FiddleOutput: React.FunctionComponent<Props> = (props: Props) => {
-  const [output, setOutput] = React.useState<string[]>([]);
+  const [logs, setLogs] = React.useState<string[]>([]);
+
   return (
     <div>
       <div>
         <button
           type="button"
           onClick={() => {
-            setOutput([]);
+            setLogs([]);
+
+            const consoleLite = createConsoleLite((...args: any[]) => {
+              const log: string | null = mapLogArgsToLog(...args);
+              if (log) {
+                setLogs((prevState) => [...prevState, log]);
+              }
+            });
 
             try {
-              window.Function(`"use strict";
-            return (console) => {
-              ${props.code}
-            };
-            `)()({
-                log: (message: string) => {
-                  setOutput((prevState) => [...prevState, message]);
-                },
-              });
+              window.Function(createFunctionCode(props.code))()(consoleLite);
             } catch (error) {
-              setOutput([String(error)]);
+              setLogs([String(error)]);
             }
           }}
         >
           Run
         </button>
       </div>
-      <pre>{output.join("\n")}</pre>
+      <pre>{logs.join("\n")}</pre>
     </div>
   );
 };
