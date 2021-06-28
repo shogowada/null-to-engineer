fb994844f86753849101b08b7cca80e80bddbb2c
Shogo Wada <shogowada@users.noreply.github.com>
2021-06-28 18:16:31 -0500

    Highlight code snippets

--- src/server/business/instruction-html-factory.ts
+++ src/server/business/instruction-html-factory.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
 import * as fs from "fs";
 import * as marked from "marked";
+import * as highlightJS from "highlight.js";
 import { Dictionary, InstructionID, InstructionIDs } from "../../common";
 
 const instructionIDToHTMLDictionary: Dictionary<string> = InstructionIDs.reduce(
@@ -9,7 +10,11 @@ const instructionIDToHTMLDictionary: Dictionary<string> = InstructionIDs.reduce(
       path.join(__dirname, "instructions", `${instructionID}.md`),
       { encoding: "utf-8" }
     );
-    const html: string = marked(markdown);
+    const html: string = marked(markdown, {
+      highlight: (code: string, language: string): string => {
+        return (highlightJS as any).highlight(code, { language }).value;
+      },
+    });
     return {
       ...dictionary,
       [instructionID]: html,
