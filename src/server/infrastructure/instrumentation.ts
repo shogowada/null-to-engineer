f7ae6ff99679637789a54ded16fe6a278aefa852
Shogo Wada <shogowada@users.noreply.github.com>
2022-02-20 08:08:59 -0600

    Remove auto instrumentation
    It isn't working for some reason. I might come back to it later.

--- src/server/infrastructure/instrumentation.ts
+++ src/server/infrastructure/instrumentation.ts
@@ -1,42 +1,28 @@
 import * as process from "process";
 import { credentials, Metadata } from "@grpc/grpc-js";
 import * as api from "@opentelemetry/api";
-import { SpanAttributes, SpanStatus, SpanStatusCode } from "@opentelemetry/api";
+import { SpanAttributes, SpanStatusCode } from "@opentelemetry/api";
 import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
-import {
-  BatchSpanProcessor,
-  ConsoleSpanExporter,
-} from "@opentelemetry/sdk-trace-base";
-import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
-import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
-import { DnsInstrumentation } from "@opentelemetry/instrumentation-dns";
-import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
-import { ExpressInstrumentation } from "@opentelemetry/instrumentation-express";
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
 import { configuration } from "./configuration";
 
 const createTracer = (honeycombAPIKey: string): api.Tracer => {
   const traceExporter = createTraceExporter(honeycombAPIKey);
   const tracerProvider = createTracerProvider(traceExporter);
 
-  tracerProvider.register();
-
-  registerInstrumentations({
-    tracerProvider,
-    instrumentations: [
-      new DnsInstrumentation(),
-      new HttpInstrumentation(),
-      new ExpressInstrumentation(),
-    ],
-  });
-
   process.on("SIGINT", async () => {
     console.log("Flushing telemetry...");
-    await tracerProvider.activeSpanProcessor.forceFlush();
-    console.log("Flushed");
-    process.exit();
+    tracerProvider.activeSpanProcessor
+      .forceFlush()
+      .then(() => console.log("Flushed telemetry"))
+      .catch((error) => console.log("Failed to flush telemetry", error))
+      .finally(() => process.exit());
   });
+
+  tracerProvider.register();
 
   return api.trace.getTracer(configuration.serviceName);
 };
@@ -54,14 +40,18 @@ const createTraceExporter = (honeycombAPIKey: string): OTLPTraceExporter => {
 };
 
 const createTracerProvider = (traceExporter: OTLPTraceExporter) => {
-  const provider = new NodeTracerProvider({
-    resource: new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: configuration.serviceName,
-    }),
+  const resource = new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: configuration.serviceName,
   });
-  provider.addSpanProcessor(new BatchSpanProcessor(traceExporter));
-  provider.addSpanProcessor(new BatchSpanProcessor(new ConsoleSpanExporter()));
-  return provider;
+
+  const tracerProvider = new NodeTracerProvider({
+    resource,
+  });
+
+  tracerProvider.addSpanProcessor(new BatchSpanProcessor(traceExporter));
+  // tracerProvider.addSpanProcessor(new BatchSpanProcessor(new ConsoleSpanExporter()));
+
+  return tracerProvider;
 };
 
 let tracer: api.Tracer | undefined;
